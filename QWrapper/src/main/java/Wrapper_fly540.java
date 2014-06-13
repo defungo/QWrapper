525e50da644a34761a3abe81be487784fa2a1107
squallyth@gmail.com <squallyth@gmail.com>
2014-06-13 23:13:33 +0800

    init

---
+++ QWrapper/src/main/java/Wrapper_fly540.java
@@ -0,0 +1,27 @@
+import com.qunar.qfwrapper.bean.booking.BookingResult;
+import com.qunar.qfwrapper.bean.search.FlightSearchParam;
+import com.qunar.qfwrapper.bean.search.ProcessResultInfo;
+import com.qunar.qfwrapper.interfaces.QunarCrawler;
+
+
+public class Wrapper_fly540 implements QunarCrawler {
+
+	@Override
+	public BookingResult getBookingInfo(FlightSearchParam arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getHtml(FlightSearchParam arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ProcessResultInfo process(String arg0, FlightSearchParam arg1) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
